5e1fa6075ddd43dff51164c24a24844c4c421ad4
MostafaGadoo <73221261+MostafaGadoo@users.noreply.github.com>
2024-11-09 14:02:36 +0200

    First commit for the apple clone website

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/HighLights";
+const App = ()  => {
+
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+
+
+    </main>
+  )
+}
+
+export default App
